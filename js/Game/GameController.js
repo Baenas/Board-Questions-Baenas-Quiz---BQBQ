4d688d202a3ccc996e14e73392d96d49bbc00405
Baenas <dbg94dani@gmail.com>
2020-02-07 23:45:29 +0100

    listo

--- js/Game/GameController.js
+++ js/Game/GameController.js
@@ -1,6 +1,6 @@
 class GameController {
 
-  constructor(numplayer, valorcasilla, primero, segundo, pause, playernow, typeq, actual, nameplayer) {
+  constructor(numplayer, valorcasilla, primero, segundo, pause, playernow, typeq, actual, nameplayer, map) {
     this.numplayer = numplayer;
     this.primero = primero;
     this.segundo = segundo;
@@ -12,11 +12,17 @@ class GameController {
     this.actual = actual;
     this.typeq = typeq;
     this.nameplayer = nameplayer;
+    this.map = map;
+    this.maxtowin = 0;
   }
   move(playerName, boxrnd) {
 
-    if (playerName.pos < 16) {
-      playerName.pos += 1;
+    if (playerName.pos == this.maxtowin && this.numplayer == 1) {
+      this.winshow(1);
+    } else if (this.numplayer == 2 && Player2.pos == this.maxtowin) {
+      this.winshow(2);
+    }
+    if (playerName.pos < this.maxtowin) {
 
 
       this.showquestion(playerName)
@@ -24,14 +30,55 @@ class GameController {
 
       this.actual = playerName.puntos;
 
-      console.log(`puntos ${playerName.name} tiene ${playerName.puntos}`)
-      this.playernow = document.querySelector(`#pos${playerName.pos} .box${playerName.id}`);
+      // console.log(`puntos ${playerName.name} tiene ${playerName.puntos}`)
+      this.playernow = document.querySelector(`#${this.map}${playerName.pos} .box${playerName.id}`);
+
+
+      console.log(`${this.maxtowin} max to win -- ${playerName.pos}`)
+
+      playerName.pos += 1;
+    }
+
+
+
+  }
+
+  winshow(playersnumber) {
+    let ganador = undefined;
+    if (playersnumber == 1) {
+      ganador = Player1;
+      document.getElementById('r1win').innerHTML = Player1.name;
+      document.getElementById('r2win').innerHTML = 'NP';
+
 
     }
-    if (playerName.pos == 16) {
-      console.log('ganador')
 
+
+    if (playersnumber == 2) {
+
+      if (Player1.puntos > Player2.puntos) {
+        ganador = Player1;
+        document.getElementById('r1win').innerHTML = Player1.name;
+        document.getElementById('r2win').innerHTML = Player2.name;
+      } else if (Player2.puntos > Player1.puntos) {
+        ganador = Player2
+        document.getElementById('r1win').innerHTML = Player2.name;
+        document.getElementById('r2win').innerHTML = Player1.name;
+      }
     }
+
+    document.getElementById('float3').classList.remove('displaynone')
+    document.getElementById('qwin').innerHTML = `Ganador ${ganador.name}`;
+
+    document.getElementById('valorreturnwin').innerHTML = "";
+
+    document.getElementById('r1').innerHTML = "";
+
+    document.getElementById('r2').innerHTML = "";
+
+    document.getElementById('r3').innerHTML = "";
+
+    document.getElementById('r4').innerHTML = "";
   }
   showquestion(player) {
 
@@ -41,12 +88,9 @@ class GameController {
     let diceto = document.getElementById('typequestion').innerHTML;
     this.typeq = Controlador.dado3();
     document.getElementById('dadoon').classList.add('displaynone')
-    if (PreguntasList.type == 'return') {
-      document.getElementById('float2').classList.remove('displaynone')
-    } else {
 
-      document.getElementById('float1').classList.remove('displaynone')
-    }
+    document.getElementById('float2').classList.remove('displaynone')
+
     document.getElementById('q').innerHTML = PreguntasList.pregunta;
 
     document.getElementById('valorreturn').innerHTML = PreguntasList.valorreturn;
@@ -72,7 +116,7 @@ class GameController {
   getanswer(id, cq) {
     if (id == cq) {
 
-      console.log(`id${id} cq${cq}`)
+
       document.getElementById('float2').classList.add('displaynone')
       // document.getElementById('float1').classList.add('displaynone')
       document.getElementById('dadoon').classList.remove('displaynone')
@@ -110,24 +154,28 @@ class GameController {
     this.numplayer += 1;
   }
   gameLoad() {
+    // document.getElementById('settings').classList.add('displaynone')
+    // document.getElementById('containergame').classList.remove('displaynone')
+    // // document.getElementById('containertablero').classList.add('displaynone')
+    // document.getElementById('containertablerosmall').classList.remove('displaynone')
+
+  }
+  mapaS() {
     document.getElementById('settings').classList.add('displaynone')
     document.getElementById('containergame').classList.remove('displaynone')
     // document.getElementById('containertablero').classList.add('displaynone')
-
-
+    document.getElementById('containertablerosmall').classList.remove('displaynone')
+    this.maxtowin = 6;
   }
-
+  mapaXL() {
+    document.getElementById('settings').classList.add('displaynone')
+    document.getElementById('containergame').classList.remove('displaynone')
+    // document.getElementById('containertablero').classList.add('displaynone')
+    document.getElementById('containertablero2').classList.remove('displaynone')
+    this.maxtowin = 16;
+  }
   generateMap1() {
-    let tablero = document.getElementById('containertablero2');
 
-    let tablerohtml = "";
-    for (let index = 0; index < 2; index++) {
-
-      tablerohtml +=
-        `
-    `
-    }
-    tablero.innerHTML += tablerohtml;
   }
   dado4() {
     let res;
@@ -161,6 +209,7 @@ class GameController {
     console.log(`- - -  1: ${this.tiradaspl1}`)
     playerturno.innerHTML = Player1.name;
     this.nameplayer = Player1;
+
   }
 
 
@@ -173,6 +222,7 @@ class GameController {
       this.segundo = 1;
       this.nameplayer = Player1;
       console.log(`- - -  1: ${this.tiradaspl1}`)
+
     } else if (this.segundo == 1) {
       playerturno.innerHTML = Player1.name;
       Controlador.move(Player2, Controlador.dado4());
@@ -181,6 +231,7 @@ class GameController {
       this.segundo = 2;
       console.log(`- - -  2: ${this.tiradaspl2}`)
       this.nameplayer = Player2;
+
     }
   }
 
@@ -203,13 +254,13 @@ class GameController {
 
 
 
-    let posboxold = document.querySelector(`#pos${ubicacion} .box${positionbox}`);
+    // let posboxold = document.querySelector(`#pos${this.map}${ubicacion} .box${positionbox}`);
 
-    let posbox = document.querySelector(`#pos${ubicacion} .box${positionbox}`);
+    let posbox = document.querySelector(`#${this.map}${ubicacion} .box${positionbox}`);
     // posbox.innerHTML += stringname;
 
 
-    if (posbox.innerHTML != "") {
+    if (posbox.style.backgroundColor == "lightgreen") {
       console.log('esta lleno')
 
       PlayerNum.pos -= 1;
@@ -224,18 +275,18 @@ class GameController {
 
 
 
+
     // posbox.style.backgroundColor = PlayerNum.color;
   }
 
 }
-Controlador = new GameController(0, 0, 1, 2, 0, "Player1")
+Controlador = new GameController(0, 0, 1, 2, 0, "Player1", 'return', undefined, undefined, 'pos1')
 
 PreguntasList = new Pregu();
 
 window.addEventListener('load', () => {
 
 
-  gamestatus.innerHTML = 'Selecciona Jugador';
 
 })
 
@@ -256,12 +307,37 @@ document.getElementById('player2addsettings').addEventListener('click', () => {
   Player2 = new Players(2, player2name.value, 0, 1, 'salmon');
 
 })
+document.getElementById('mapsettingssmall').addEventListener('click', () => {
+  if (document.getElementById('mapsettingsbig').style.backgroundColor == "lightgreen") {
+    document.getElementById('mapsettingsbig').style.backgroundColor = "rgb(114, 185, 185)"
+    document.getElementById('mapsettingssmall').style.backgroundColor = "lightgreen";
 
+  } else {
+    document.getElementById('mapsettingssmall').style.backgroundColor = "lightgreen";
+    Controlador.map = 'pos1';
+  }
+
+})
+document.getElementById('mapsettingsbig').addEventListener('click', () => {
+  if (document.getElementById('mapsettingssmall').style.backgroundColor == "lightgreen") {
+    document.getElementById('mapsettingssmall').style.backgroundColor = "rgb(114, 185, 185)"
+    document.getElementById('mapsettingsbig').style.backgroundColor = "lightgreen";
+
+  } else {
+    document.getElementById('mapsettingsbig').style.backgroundColor = "lightgreen";
+    Controlador.map = 'pos2';
+  }
+
+})
 document.getElementById('btnLoadGame').addEventListener('click', () => {
-  Controlador.gameLoad()
-  Controlador.generateMap1()
 
 
+  if (Controlador.map == 'pos1') {
+    Controlador.mapaS()
+  } else if (Controlador.map == 'pos2') {
+    Controlador.mapaXL()
+
+  }
 
 
 
--- js/Game/list.js
+++ js/Game/list.js
@@ -1,5 +1,5 @@
 class Pregu {
-    constructor(type, id, preguntagen, r1, r2, r3, r4, valorreturn, cq) {
+    constructor(type, id, pregunta, r1, r2, r3, r4, valorreturn, cq) {
         this.id = id;
         this.type = type;
         this.r1 = r1;
@@ -8,7 +8,7 @@ class Pregu {
         this.r4 = r4;
         this.valorreturn = valorreturn
         this.cq = cq;
-        this.preguntagen = preguntagen;
+        this.pregunta = pregunta;
         // this.tesq = [
         //     { type: 'return', id: this.id, pregunta: "1 + 1", r1: 1, r2: 2, r3: 11, r4: 0, cq: "r2" }
 
@@ -18,6 +18,7 @@ class Pregu {
         //     { id: 1, pregunta: "2 + 1", r1: 1, r2: 2, r3: 3, r4: 0, cq: "r3" }
 
         // ]
+        this.idlist = [];
         this.js = [
             {
                 type: 'return', id: 1, pregunta: `if(a = undefined){  ${`<br>`} return a${`<br>`}}`, valorreturn: 'Que valor tiene a', r1: 'true', r2: 'false', r3: 'undefined', r4: 'null', cq: "r3"
@@ -33,6 +34,16 @@ class Pregu {
             },
             {
                 type: 'return', id: 5, pregunta: ` let name = "Daniel" ${`<br>`} name++ ${`<br>`}   `, valorreturn: 'Que valor tiene name', r1: 'Daniel1', r2: 'DanielDaniel', r3: 'NaN', r4: 'null', cq: "r3"
+            }, {
+                type: 'return', id: 6, pregunta: ` let a = [1,2,3]" ${`<br>`} b = a[2] + a ${`<br>`}   `, valorreturn: 'Que valor tiene b', r1: 'a[2][1,2,3]', r2: '31,2,3', r3: '3,1,2,3', r4: '3 [1,2,3]', cq: "r2"
+            }, {
+                type: 'return', id: 7, pregunta: ` let u = ['abc','def'] " ${`<br>`} u = u[1] += u[0]; ${`<br>`}   `, valorreturn: 'Que valor tiene u', r1: 'abcdef', r2: "defabcdef", r3: '["abc", "defabc"]', r4: "defabc", cq: "r4"
+            }, {
+                type: 'return', id: 8, pregunta: ` let str = "Daniel!"; " ${`<br>`}let  res = str[3+1].toUpperCase(); ${`<br>`}   `, valorreturn: 'Que vale STR', r1: 'e', r2: "i", r3: 'l', r4: "E", cq: "r1"
+            }, {
+                type: 'return', id: 9, pregunta: `var str= ["a", "b", "c", "d", "e"]; ${`<br>`}var mod = str.slice(1, 3-1);${`<br>`}   `, valorreturn: 'Que vale STR', r1: 'c', r2: "b", r3: 'd', r4: "e", cq: "r2"
+            }, {
+                type: 'return', id: 10, pregunta: `var str= [["a", "b"],['1','2']]; ${`<br>`}var mod = str.slice(1, 1);${`<br>`}   `, valorreturn: 'Que vale mod', r1: '[["1"]]', r2: '[["1", "2"]]', r3: '[]', r4: "a", cq: "r3"
             },
 
 
@@ -40,23 +51,42 @@ class Pregu {
 
 
 
+        this.res = this.js.sort(function () {
+            return 0.5 - Math.random();
+        });
+
+        this.incremento = 0;
 
     }
 
-    getquestion(type) {
-        this.random = Math.floor(Math.random() * this.js.length);
-        this.type = this.js[this.random].type
-        this.pregunta = this.js[this.random].pregunta
-        this.r1 = this.js[this.random].r1
-        this.r2 = this.js[this.random].r2
-        this.r3 = this.js[this.random].r3
-        this.r4 = this.js[this.random].r4
-
-        this.cq = this.js[this.random].cq
-        this.valorreturn = this.js[this.random].valorreturn
-        console.log(this.js[this.random])
+    getquestion() {
 
 
+
+        let valores = this.res.slice(this.js, 100)
+
+
+        this.type = valores[this.incremento].type;
+        this.pregunta = valores[this.incremento].pregunta
+        this.r1 = valores[this.incremento].r1
+        this.r2 = valores[this.incremento].r2
+        this.r3 = valores[this.incremento].r3
+        this.r4 = valores[this.incremento].r4
+
+        this.id = valores[this.incremento].d;
+        this.cq = valores[this.incremento].cq
+        this.valorreturn = valores[this.incremento].valorreturn
+        console.log(valores)
+        console.log(valores[this.incremento].pregunta);
+
+        // this.idlist.push(this.js[this.random].id)
+
+
+        this.incremento++;
+
+
+
+        // Sconsole.log(this.idlist[0])
 
 
 
